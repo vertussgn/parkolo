60012bf442b0784248e8dc06fb1f1107719a4f48
vertussgn <vertussgn@gmail.com>
2025-04-02 19:56:14 +0200

    0.1

---
+++ src/test/java/com/example/parkolo/ParkoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
