a659a1e3b3e9d8f0afd32a0bdd6eaf36baff8d25
vertussgn <vertussgn@gmail.com>
2025-05-09 16:57:35 +0200

    0.81

--- src/main/java/com/example/parkolo/controller/ViewController.java
+++ src/main/java/com/example/parkolo/controller/ViewController.java
@@ -8,12 +8,16 @@ public class ViewController {
 
     @GetMapping("/status")
     public String showStatusPage() {
-        return "status"; // Ez a név megegyezik a status.html fájl nevével (templates/status.html)
+        return "status"; // Ez a név megegyezik a status.html fájl nevével
     }
 
     @GetMapping("/index")
     public String showIndexPage() {
         return "index"; // Ez a név megegyezik az index.html fájl nevével
     }
-}
 
+    @GetMapping("/")
+    public String redirectToIndex() {
+        return "redirect:/index";
+    }
+}
